375fe09321644b9201e9370769f702bb1b6cbf9d
lenovo <lenovo@lenovo-PC>
2024-01-20 09:48:10 +0530

    commit

---
+++ AssesmentHomework/src/com/edubridge/assesment/FloatInput.java
@@ -0,0 +1,11 @@
+package com.edubridge.assesment;
+import java.util.Scanner;
+public class FloatInput {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		Float  a=s.nextFloat();
+		System.out.printf("%.2f",a);
+	}
+
+}
