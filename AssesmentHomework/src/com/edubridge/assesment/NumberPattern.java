534d833fb9d95536189aee12bfa860fa28d76da0
lenovo <lenovo@lenovo-PC>
2024-01-23 10:45:20 +0530

    commit

---
+++ AssesmentHomework/src/com/edubridge/assesment/NumberPattern.java
@@ -0,0 +1,22 @@
+package com.edubridge.assesment;
+
+import java.util.Scanner;
+
+public class NumberPattern {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int x=1;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(x++ +" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ AssesmentHomework/src/com/edubridge/assesment/Unoid.java
@@ -0,0 +1,13 @@
+package com.edubridge.assesment;
+
+import java.util.Scanner;
+
+public class Unoid {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char a=s.next().charAt(0);
+		System.out.println((int)a);
+	}
+
+}
